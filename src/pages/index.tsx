79429ac93a6aa1756b7bdad955bbf27e2d7d3cf0
Ryan <ryand@croftventures.com>
2021-07-13 14:42:43 -0500

    swr test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,17 @@
+import { Button, ButtonGroup, Container, Heading, Stack } from "@chakra-ui/react"
+import React from "react"
 
-const Index = () => (
-  <div>POG</div>
-)
+const Index = () => {
+  return (
+    <Stack spacing={8}>
+      <Container centerContent>
+        <Heading fontSize="xl">Miru</Heading>
+      </Container>
+      <Container centerContent>
+        <a href="/api/login/anilist"><Button>Login</Button></a>
+      </Container>
+    </Stack>
+  )
+}
 
 export default Index
