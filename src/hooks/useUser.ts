710e6d7ae38baec24f605979f0d1f390c93ce953
Ryan <ryand@croftventures.com>
2021-07-16 15:14:01 -0500

    dashboard start

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -4,7 +4,7 @@ import { fetcher } from "../utils/fetcher";
 export default function useUser(){
     const { data, mutate, error } = useSWR('/api/anilist/user');
 
-    const loading = !data && !error;
+    const loading = !data;
     const loggedOut = data && data.errors;
 
     return {
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,24 +1,45 @@
-import { Grid, GridItem } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Grid, GridItem, Image } from '@chakra-ui/react'
 import useUser from '../hooks/useUser'
 import React from 'react'
 import { useEffect } from 'react'
 import Router from 'next/router'
 
 const Dashboard = () => {
-    const { user, mutate, loggedOut } = useUser();
+    const { user, mutate, loading, loggedOut } = useUser();
 
     useEffect(() => {
         if(loggedOut && user){
             Router.replace("/")
         }
+        console.log(user)
     }, [user, loggedOut])
 
+
     return(
-        <Grid>
-            <GridItem>
-                {}
-            </GridItem>
-        </Grid>
+        <>
+        {loading ? <div>Loading...</div> :
+        (
+            <Grid
+            h="100vh"
+            templateRows="repeat(1,1fr)"
+            templateColumns="repeat(12,1fr)"
+            >
+                <GridItem colSpan={3} bg="white">
+                    <Box textAlign="center">
+                        <Image 
+                            src={user.data.Viewer.avatar.large}
+                            borderRadius="full"
+                            boxSize="300px"
+                        />
+                    </Box>
+                </GridItem>  
+                <GridItem colSpan={9} bg="black">
+
+                </GridItem>
+                
+            </Grid>
+        )}
+        </>
     )
 }
 
