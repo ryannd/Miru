2bbf5be967321c59c474f742e1edd8f147e56264
Ryan <ryand@croftventures.com>
2021-07-16 11:23:56 -0500

    oops forgot dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,10 +1,18 @@
 import { Grid, GridItem } from '@chakra-ui/react'
 import useUser from '../hooks/useUser'
 import React from 'react'
-import useSWR from 'swr'
+import { useEffect } from 'react'
+import Router from 'next/router'
 
 const Dashboard = () => {
     const { user, mutate, loggedOut } = useUser();
+
+    useEffect(() => {
+        if(loggedOut && user){
+            Router.replace("/")
+        }
+    }, [user, loggedOut])
+
     return(
         <Grid>
             <GridItem>
